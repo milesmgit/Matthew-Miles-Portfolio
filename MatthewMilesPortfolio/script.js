9c0e777f0d0686c9ad5a26465d118006fe905406
matthewmiles <milesm427@gmail.com>
2020-06-02 20:09:06 -0400

    Initial Commit

---
+++ MatthewMilesPortfolio/script.js
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+
+
+//// function to choose background image on click
+//const backgroundArray = ['url(img/teal-background.jpg)', 'url(img/blue-background.jpg)', 'url(img/purple-background.jpg)']
+//// grab button to add one to counter so i can display different backgrounds.
+//const changeBackground = document.querySelector('#changeBackground');
+//const body = document.querySelector('body');
+//let counter = 0;
+//changeBackground.addEventListener('click', function () {
+//    counter++;
+//    if (counter > backgroundArray.length - 1) {
+//        counter = 0;
+//    }
+//    body.style.backgroundImage = backgroundArray[counter];
+//});
+
+
+
+
+    // grabbing grid list so i can work with it to add user choice of x or o.
+    const gridArray = document.querySelectorAll('.grid');
+    const parentDiv = document.querySelectorAll('.parentDiv');
+
+
+
+
+
+
+
