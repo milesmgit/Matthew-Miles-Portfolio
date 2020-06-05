aaab89ee2575d205804800c27c7efe98887ad305
matthewmiles <milesm427@gmail.com>
2020-06-05 15:51:53 -0400

    various css enhancements; tweaked about me page

--- script.js
+++ script.js
@@ -51,3 +51,35 @@ function rollout() {
     techLouHref2.href = 'https://www.techlouisville.org';
     techLouHref2.target = "_blank";
 }
+
+let bio = document.getElementById('about-main-content');
+let wrapper = document.createElement('div');
+
+function displayBio() {
+  bio.innerHTML = '';
+  bio.appendChild(wrapper);
+  wrapper.id = "wrapperId";
+  wrapper.innerHTML = `<h1 id='bioH1'>Hello</h1>
+                  <p id='bioP1'>Matthew Miles is from Louisville, Ky U.S.A and currently lives in the downtown area.
+                  </p>
+                  <button id='resetButton' onclick='resetAboutMe()'</button>`
+
+}
+
+
+function resetAboutMe() {
+  bio.innerHTML = `<div id="main-flex">
+    <div class="card">
+      <div class="resources"><p>Resume</p><a href="https://drive.google.com/file/d/1-tlYJYkcMLbY3jegKX5seRLQ8ff6mQgM/view?usp=sharing" target="_blank"><img src="img/google-drive.jpeg"  alt="Matthew Miles: Resume" width="150" height="150" title="Click to view/download resume (Google Drive):  https://drive.google.com/file/d/1-tlYJYkcMLbY3jegKX5seRLQ8ff6mQgM/view?usp=sharing"></a></div>
+    </div>
+    <div class="card">
+      <div class="resources"><p>LinkedIn Profile</p><a href="https://linkedin.com/in/matthew-miles-code-louisville" target="_blank"><img src="img/linked.jpeg"  alt="Matthew Miles: Linked-In Profile" width="155" height="155" title="Click to view my Linked-In Profile:  https://linkedin.com/in/matthew-miles-code-louisville"></a></div>
+    </div>
+    <div class="card">
+      <div class="resources"><p>GitHub Profile</p><a href="https://github.com/milesmgit?tab=repositories" target="_blank"><img src="img/github.png"  alt="Matthew Miles: GitHub" width="170" height="170" title="Click to view GitHub Profile:  https://github.com/milesmgit?tab=repositories"></a></div>
+    </div>
+    <div class="card">
+      <div class="resources"><p>Bio</p><img id="matt-bio" src="img/matt-miles.jpeg"  onclick="displayBio()" alt="Matthew Miles: Bio" width="170" height="170" title="Click to view Bio:  https://github.com/milesmgit?tab=repositories"></a></div>
+    </div>
+  </div>`
+}
